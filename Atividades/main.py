9fdab664139e0b8cb033b199bbf4f96b3752b2d5
Cauã Olive Barbosa <cauaolivebarbosa@gmail.com>
2025-04-25 11:25:25 -0300

    Concluded

--- Atividades/main.py
+++ Atividades/main.py
@@ -48,7 +48,7 @@ def read_gods(db: Session = Depends(get_db)):
 def read_god(god_id: int, db: Session = Depends(get_db)):
     god = crud.get_god(db, god_id)
     if not god:
-        raise HTTPException(status_code=404, detail="Deus não encontrado")
+        raise HTTPException(status_code=404, detail="God don't finded")
     return god
 
 @app.post("/gods", response_model=schemas.GodOut)
@@ -59,7 +59,7 @@ def create_god(god: schemas.GodCreate, db: Session = Depends(get_db)):
 def update_god(god_id: int, new_data: schemas.GodCreate, db: Session = Depends(get_db)):
     db_god = db.query(models.God).filter(models.God.id == god_id).first()
     if not db_god:
-        raise HTTPException(status_code=404, detail="Deus não encontrado")
+        raise HTTPException(status_code=404, detail="God don't finded")
     
     for field, value in new_data.dict().items():
         setattr(db_god, field, value)
@@ -73,7 +73,7 @@ def update_god(god_id: int, new_data: schemas.GodCreate, db: Session = Depends(g
 def patch_god(god_id: int, god_update: schemas.GodUpdate, db: Session = Depends(get_db)):
     db_god = db.query(models.God).filter(models.God.id == god_id).first()
     if not db_god:
-        raise HTTPException(status_code=404, detail="Deus não encontrado")
+        raise HTTPException(status_code=404, detail="God don't finded")
     
     for field, value in god_update.dict(exclude_unset=True).items():
         setattr(db_god, field, value)
@@ -87,9 +87,9 @@ def patch_god(god_id: int, god_update: schemas.GodUpdate, db: Session = Depends(
 def delete_god(god_id: int, db: Session = Depends(get_db)):
     db_god = crud.get_god(db, god_id)
     if not db_god:
-        raise HTTPException(status_code=404, detail="Deus não encontrado")
+        raise HTTPException(status_code=404, detail="God don't finded")
     crud.delete_god(db, db_god)
-    return {"message": "Deus removido com sucesso"}
+    return {"message": "God removed with sucess"}
 
 @app.get("/gods-page", response_class=HTMLResponse)
 def show_gods(request: Request, db: Session = Depends(get_db)):
